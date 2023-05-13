430a702e113d2ac7165eeed88ee3a37fd01d1006
Abdalrhman M. Alkady <alkady2019@gmail.com>
2023-05-13 15:49:05 +0300

    Readme File init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# To-Do-List-App
- To-List-App using pure php
+# To-Do List App
+
+This is a simple web-based to-do list application developed using PHP, Bootstrap, and MySQL. The project was developed as a practical assignment for the university.
+
+## Requirements
+
+* PHP 7.2 or higher
+* MySQL 5.7 or higher
+* Web server (Apache or Nginx)
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/Phoenix-H22/To-Do-List-App.git`
+2. Create a new MySQL database and import the `task.sql` file included in the project.
+3. Update the database configuration in `db.php` with your database credentials.
+4. Start your web server and navigate to the project folder.
+
+## Usage
+
+The application allows users to create a to-do list and add tasks to it. Users can mark tasks as completed or delete them from the list.
+
+## Contributing
+
+Contributions are always welcome! If you find a bug or want to add a new feature, feel free to create a pull request.
---
+++ database/task.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 13, 2023 at 12:48 PM
+-- Server version: 8.0.30
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `to_do`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `id` int NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
